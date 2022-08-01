b9ac3ce95dd05b85545e2c6e5f3de379f0027bc9
Iwancof <rock.iwancof@gmail.com>
2022-08-01 21:52:57 +0900

    remove old defined

--- src/main.rs
+++ src/main.rs
@@ -38,51 +38,6 @@ define_prim_wrap!(
     [HIPROC, 0xffff],
 );
 
-/*
-#[repr(C)]
-#[derive(Clone, Copy)]
-struct ElfType {
-    inner: u16,
-}
-
-impl ElfType {
-    const NONE: u16 = 0;
-    const REL: u16 = 1;
-    const EXEC: u16 = 2;
-    const DYN: u16 = 3;
-    const CORE: u16 = 4;
-    const LOOS: u16 = 0xfe00;
-    const HIOS: u16 = 0xfeff;
-    const LOPROC: u16 = 0xff00;
-    const HIPROC: u16 = 0xffff;
-}
-
-impl core::fmt::Display for ElfType {
-    fn fmt(&self, fmt: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        let s = match self.inner {
-            Self::NONE => "None",
-            Self::REL => "Rel",
-            Self::EXEC => "Exec",
-            Self::DYN => "Dyn",
-            Self::CORE => "Core",
-            Self::LOOS => "Loos",
-            Self::HIOS => "Hios",
-            Self::LOPROC => "Loproc",
-            Self::HIPROC => "Hiproc",
-            _ => "Unknown",
-        };
-
-        write!(fmt, "{}", s)
-    }
-}
-
-impl core::fmt::Debug for ElfType {
-    fn fmt(&self, fmt: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(fmt, "{}", self.inner)
-    }
-}
-*/
-
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
 struct ElfHeader {
